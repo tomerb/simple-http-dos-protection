342fad9ea1ce44ecd3970de557b01bcbcb36c2d9
Tomer Barletz <barletz@gmail.com>
2023-06-29 18:09:51 +0300

    Server now serves properly

--- dos_server.cpp
+++ dos_server.cpp
@@ -25,6 +25,15 @@ static atomic<bool> exit_thread_flag{false};
 // client_id -> last incoming requests for that client
 static map<int, array<long, 5>> last_requests;
 
+static const string HTTP_OK = "HTTP/1.1 200 OK\r\n"                  \
+                              "Content-Type: application/json\r\n"   \
+                              "Content-Length: 21\r\n"               \
+                              "{\"result\":\"allowed\"}";
+static const string HTTP_SERVICE_UNAVAILABLE = "HTTP/1.1 503 Service Unavailable\r\n" \
+                              "Content-Type: application/json\r\n"                    \
+                              "Content-Length: 25\r\n"                                \
+                              "{\"result\":\"not allowed\"}";
+
 struct IncomingRequest
 {
     int client_id;
@@ -34,6 +43,32 @@ struct IncomingRequest
 static bool allow_request()
 {
     return false;
+}
+
+static bool send_response(int socket, int client_id)
+{
+    const string &response = client_id == 0 ? HTTP_OK : HTTP_SERVICE_UNAVAILABLE;
+    int bytes_sent = send(socket, response.c_str(), response.length(), MSG_NOSIGNAL);
+    return bytes_sent == response.length();
+}
+
+static int extract_client_id(const string &request)
+{
+    const string client_id_str = "?clientId=";
+    auto client_id_idx = request.find_first_of(client_id_str);
+    auto first_line_end_idx = request.find_first_of("\r\n");
+    if (request.rfind("GET", 0) == 0 &&
+        client_id_idx != string::npos &&
+        first_line_end_idx != string::npos &&
+        client_id_idx < first_line_end_idx)
+    {
+        auto client_id_pos = client_id_idx + client_id_str.size();
+        auto client_id_str = request.substr(client_id_pos,
+                                            first_line_end_idx - client_id_pos);
+        return stoi(client_id_str);
+    }
+
+    return -1;
 }
 
 static void http_listen()
@@ -46,6 +81,14 @@ static void http_listen()
     }
 
     cout << "Socket " << sockfd << " created" << endl;
+
+    const int enable = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    {
+        cout << "Unable to set socket option" << endl;
+        close(sockfd);
+        return;
+    }
 
     sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -76,6 +119,41 @@ static void http_listen()
     cout << "Got new connection from " << inet_ntoa(client_addr.sin_addr)
          << ":" << ntohs(client_addr.sin_port) << endl;
 
+    char buffer[256];
+    int bytes_read = read(newsockfd, buffer, 255);
+    if (bytes_read < 0)
+    {
+        cout << "Unable to read from netsockfd" << endl;
+        close(newsockfd);
+        close(sockfd);
+        return;
+    }
+    else
+    {
+        cout << "Got from client: " << buffer << endl;
+    }
+
+    string request(buffer, bytes_read);
+
+    int client_id_from_request = extract_client_id(request);
+    if (client_id_from_request < 0)
+    {
+        cout << "Illegal client request: " << request << endl;
+        close(newsockfd);
+        close(sockfd);
+        return;
+    }
+
+    if (!send_response(newsockfd, client_id_from_request))
+    {
+        cout << "Unable to send response to client" << endl;
+        close(newsockfd);
+        close(sockfd);
+        return;
+    }
+
+    cout << "Done handling request" << endl;
+
     close(newsockfd);
     close(sockfd);
 }
@@ -85,6 +163,7 @@ static void do_serve()
     while (!exit_thread_flag)
     {
         http_listen();
+        sleep(1);
     }
 }
 
